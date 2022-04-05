c964268c12d5d9016f46b3a7f1ca462d188499a0
yuriyuri1704 <haruonma1117@gmail.com>
2022-04-05 17:55:06 +0900

    追加：ジャンプ機能

---
+++ Assets/Scripts/IsGround.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Ray‚Å“–‚½‚Á‚½‚à‚Ì‚Ì”»’è‚ªGround‚©”»’f‚·‚é
+/// </summary>
+public class IsGround : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,11 +6,25 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] float speed = 1.0f;
 
-    private void Update()
+    /// <summary>
+    /// リジッドボディを取得する変数
+    /// </summary>
+    private Rigidbody2D rb;
+    private bool isGround;
+    [SerializeField] float upForce = 200f; //上方向にかける力
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+    private void FixedUpdate()
     {
         Move();
+        Jump();
 
     }
+
+
 
     /// <summary>
     /// 矢印キーで左右に移動するメソッド
@@ -26,6 +40,33 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position -= transform.right * speed * Time.deltaTime;
+        }
+    }
+
+    /// <summary>
+    /// スペースキーでジャンプするメソッド
+    /// </summary>
+    private void Jump() 
+    {
+        if (isGround == true)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                isGround = false;
+                rb.AddForce(new Vector2(0, upForce)); //上に向かって力を加える
+            }
+        }
+    }
+
+    /// <summary>
+    /// Groundタグのオブジェクトに触れたときisGroundをtrueにするメソッド
+    /// </summary>
+    /// <param name="other"></param>
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground") 
+        {
+            isGround = true; 
         }
     }
 
